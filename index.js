806e16a407beeeb59f417b344ec02318e15f34a5
DuMark Allman <dumark.allman@yahoo.com>
2023-04-24 00:16:10 -0400

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
 }
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  I need to make an isPalindrome function that returns either true or false. 
+  When the input string is the same forwards and backwards, I should return true. 
+  That means that if the input string is the same after I reverse it,
+   I should return true. For instance, 
+  "mom" in reverse is also "mom", and "racecar" in reverse is also "racecar", 
+  so my solution should return true for these cases. 
+  "hi" in reverse is "ih", 
+  so my solution should return false for this case.
 */
 
 // You can run `node index.js` to view these console logs
